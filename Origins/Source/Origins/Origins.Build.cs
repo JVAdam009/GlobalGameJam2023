0ac56ac21a56c991d0b9a2adf325d56409ec0a37
Matthew Steinhardt <Matthew.steinhardt@gmail.com>
2023-01-31 14:34:55 -0800

    Initial Project Upload

---
+++ Origins/Source/Origins/Origins.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Origins : ModuleRules
+{
+	public Origins(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
